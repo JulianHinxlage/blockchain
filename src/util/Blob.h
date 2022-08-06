d471fee859f057032e2164ec105f27cee51bce32
Julian Hinxlage <hinxlage.julian@web.de>
2022-08-06 12:07:05 +0200

    added basic cryptography

---
+++ src/util/Blob.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <cstdint>
+
+template<int> struct BlobWord {};
+template<> struct BlobWord<0> { typedef uint8_t Type; };
+template<> struct BlobWord<1> { typedef uint16_t Type; };
+template<> struct BlobWord<2> { typedef uint32_t Type; };
+template<> struct BlobWord<3> { typedef uint64_t Type; };
+
+template<int bitCount>
+struct Blob {
+public:
+	static const int byteCount = ((bitCount - 1) / 8) + 1;
+	typedef typename BlobWord<(byteCount % 2 == 0) + (byteCount % 4 == 0) + (byteCount % 8 == 0)>::Type Word;
+	static const int wordCount = ((byteCount - 1) / sizeof(Word)) + 1;
+	union {
+		Word words[wordCount];
+		uint8_t bytes[byteCount];
+	};
+
+    Blob() {}
+
+    template<typename T>
+    Blob& operator=(const T& t) {
+        int size = sizeof(*this) < sizeof(t) ? sizeof(*this) : sizeof(t);
+        for (int i = 0; i < size; i++) {
+            bytes[i] = ((uint8_t*)&t)[i];
+        }
+        for (int i = size; i < sizeof(*this); i++) {
+            bytes[i] = 0;
+        }
+        return *this;
+    }
+
+    template<typename T>
+    explicit operator T() const {
+        T t;
+        int size = sizeof(*this) < sizeof(t) ? sizeof(*this) : sizeof(t);
+        for (int i = 0; i < size; i++) {
+            ((uint8_t*)&t)[i] = bytes[i];
+        }
+        for (int i = size; i < sizeof(t); i++) {
+            ((uint8_t*)&t)[i] = 0;
+        }
+        return t;
+    }
+
+    template<typename T>
+    explicit Blob(const T& t) {
+        operator=(t);
+    }
+};
